3a82c8427bc96395cd6b095b2e40f7192609afc1
Seongkwan Yoon <lingostar.joker@gmail.com>
2013-04-04 13:58:20 +0900

    Step 1 In Book
    Initial Commit.
    Simple single pomodoro with alert

---
+++ PomoTimer/LSAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  LSAppDelegate.h
+//  PomoTimer
+//
+//  Created by Lingostar on 13. 1. 14..
+//  Copyright (c) 2013년 Lingostar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class LSViewController;
+
+@interface LSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) LSViewController *viewController;
+
+@end
---
+++ PomoTimer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PomoTimer
+//
+//  Created by Lingostar on 13. 1. 14..
+//  Copyright (c) 2013년 Lingostar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LSAppDelegate class]));
+    }
+}
